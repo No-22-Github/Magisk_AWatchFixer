5e57707d10041de67dc1387d20e0d26704634e28
No.22 <132265925+No-22-Github@users.noreply.github.com>
2025-02-14 17:12:58 +0800

    update action.sh

--- action.sh
+++ action.sh
@@ -8,6 +8,34 @@ read_config() {
   echo ${result:-$2}
 }
 
+parse_config() {
+  case "$1" in
+    "0")
+      echo "关机"
+      ;;
+    "1")
+      echo "不执行任何操作"
+      ;;
+    "2")
+      echo."禁用所有 Magisk 模块"
+      ;;
+    "3")
+      echo "进入 Recovery"
+      ;;
+    "4")
+      echo "进入 Fastboot"
+      ;;
+    "5")
+      echo "进入 深刷模式 (EDL)"
+      ;;
+    *)
+      echo "未知选项，请检查配置文件。原数字：$1"
+      ;;
+}
+    
+    
+  
+  
 echo "正在读取当前配置文件..."
 sleep 0.3
 FAIL_1=$(read_config "FAIL_1=" "None")
@@ -17,4 +45,6 @@ FAIL_4=$(read_config "FAIL_4=" "None")
 FAIL_5=$(read_config "FAIL_5=" "None")
 TIMEOUT=$(read_config "TIMEOUT=" "None")
 echo "读取完毕！解析中..."
-
+echo "当前救砖策略："
+echo " "
+echo -n "第一次启动失败："
